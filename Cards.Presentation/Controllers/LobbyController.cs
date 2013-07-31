b7ac2eea69a6052f8f630cbe2d752972a74d15dc
pellebjerkestrand <pelle@pellebjerkestrand.net>
2013-07-31 17:31:15 +0200

    Lobby and Game views and controllers added

---
+++ Cards.Presentation/Controllers/LobbyController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Cards.Presentation.Controllers
+{
+    public class LobbyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
